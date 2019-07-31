4b41ca7ef3f935ac04bf1ba4c9d2398c292d791c
Tanner Gooding <tagoo@outlook.com>
2019-07-31 12:25:47 -0700

    Renaming bits from example to minecraftsharp

--- src/main/java/com/tannergooding/minecraftsharp/MinecraftSharp.java
+++ src/main/java/com/tannergooding/minecraftsharp/MinecraftSharp.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.tannergooding.minecraftsharp;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MinecraftSharp.MODID, name = MinecraftSharp.NAME, version = MinecraftSharp.VERSION)
+public class MinecraftSharp
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "minecraftsharp";
+    public static final String NAME = "MinecraftSharp";
+    public static final String VERSION = "0.1";
 
     private static Logger logger;
 
